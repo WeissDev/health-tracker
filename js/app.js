ac83f12c930a9251bd73d52dc1de8a355f8275e5
WeissDev <valentin.weiss@hotmail.com>
2015-08-21 16:10:28 +0200

    Revert "revert"
    This reverts commit 3a3e70a1aaff7d415ab62bff31d8bb6f4f6ea848.

--- js/app.js
+++ js/app.js
@@ -10,14 +10,7 @@ app.FoodModel = Backbone.Model.extend({
 		name: '',
 		brand: '',
 		calories: '',
-		selected: false
-	},
-
-	/** Toggle the checked state of this model */
-	checkedState: function() {
-		this.save({
-			selected: !this.get('selected');
-		});
+		checked: false
 	}
 });
 
@@ -29,29 +22,15 @@ var app = app || {};
 
 app.FoodCollection = Backbone.Collection.extend({
 	/** Reference to this collections model */
-	model: app.FoodModel,
+	model: app.FoodModel
 
-	selected: function() {
-		return this.filter(function(item) {
-			return item.get('selected');
-		});
-	},
+});
 
-	remaining: function() {
-		return this.without.apply( this, this.selected() );
-	},
+var app = app || {};
+/** Collection for selected food items */
+app.SelectedCollection = Backbone.Collection.extend({
 
-	nextOrder: function() {
-		if ( !this.length ) {
-			return 1;
-		}
-		return this.last().get('order') + 1;
-	},
-
-	comparator: function(item) {
-		return item.get('order');
-	}
-
+	model: app.FoodModel
 });
 
 /**
@@ -61,22 +40,14 @@ app.FoodCollection = Backbone.Collection.extend({
 var app = app || {};
 
 /** View for search results */
-
 app.FoodView = Backbone.View.extend({
 
 	el: '.search-results',
 	template: _.template( $('#search-results-template').html() ),
 
-	events: {
-		'click .checkbox': 'addToSelected'
-	},
-
-
 	render: function() {
-		/** this.el refers to table.search-results */
-		this.generateId()
+		/** this.el refers to ul#search-results */
 		this.$el.append( this.template(this.model.attributes) );
-		console.log(this.model.attributes);
 
 		return this;
 	}
@@ -99,14 +70,19 @@ app.SelectedFoodView = Backbone.View.extend({
 
 var app = app || {};
 
-
+/** View for users list of food items */
 app.ListView = Backbone.View.extend({
 
 	el: '#results',
 
+	events: {
+		'click .add': 'addToSelected'
+	},
+
 	initialize: function(results) {
 		this.collection = new app.FoodCollection(results);
-
+		this.resultsArray = results;
+		console.log(this.resultsArray[0]);
 		this.render();
 	},
 
@@ -123,37 +99,17 @@ app.ListView = Backbone.View.extend({
 			model: result
 		});
 		this.$el.append( foodView.render().el );
-	}
-
-});
-
-// Todo Router
-// ----------
-var app = app || {};
-
-var Workspace = Backbone.Router.extend({
-
-	routes: {
-		'*filter': 'setFilter'
 	},
 
-	setFilter: function( param ) {
-		// Set the current filter to be used
-		if ( param ) {
-			param = param.trim();
-		}
-		app.TodoFilter = param || '';
+	addToSelected: function(index) {
+		for (var i = 0; i < this.resultsArray.length; i++) {
+			this.resultsArray[i].btnId = i;
 
-		// Trigger a collection filter event, causing hiding/unhiding
-		// of Food view items
-		app.Todos.trigger('filter');
+		};
+
 	}
+
 });
-
-app.TodoRouter = new Workspace();
-Backbone.history.start();
-
-
 
 /**
  * MAIN APP
@@ -163,11 +119,15 @@ var app = app || {};
 
 $(function() {
 
+
+
+	//
+
 	$('#search-btn').on('click', function() {
 		var searchResults = [];
 		$('.search-results').empty();
 		var userInput = $('#search-bar').val();
-
+		console.log(userInput);
 		var nutritionixUrl = 'https://api.nutritionix.com/v1_1/search/' + userInput + '?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=7609e232&appKey=0a249bb0ad1fc18455fde567706ebba7'
 
 		$.getJSON(nutritionixUrl, function(data) {
@@ -177,10 +137,10 @@ $(function() {
 				searchResult.name = responseArray[i].fields.item_name;
 				searchResult.brand = responseArray[i].fields.brand_name;
 				searchResult.calories = responseArray[i].fields.nf_calories;
-				searchResult.selected = false;
+				searchResult.checked = false;
 				searchResults.push(searchResult);
 			}
-
+			console.log(searchResults);
 			new app.ListView(searchResults);
 		});
 
