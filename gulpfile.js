94ac5185aba037903b98ad121a6253a723ae0b9f
WeissDev <valentin.weiss@hotmail.com>
2015-08-24 02:12:22 +0200

    setup gulp

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    livereload = require('gulp-livereload'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    del = require('del');
+
+// Styles
+gulp.task('styles', function() {
+  return gulp.src('css/main.css')
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src([
+    'js/models/model.js',
+    'js/collections/selectedcollection.js',
+    'js/collection/resultcollection.js',
+    'js/views/selectedfoodview.js',
+    'js/views/foodview.js',
+    'js/views/appview.js',
+    'js/app.js'
+    ])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+gulp.task('clean', function(cb) {
+    del(['dist/css', 'dist/js'], cb)
+});
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images');
+});
+
+// Watch
+gulp.task('watch', function() {
+
+  // Watch .scss files
+  gulp.watch('css/*.css', ['styles']);
+
+  // Watch .js files
+  gulp.watch('js/*.js', ['scripts']);
+
+  // Create LiveReload server
+  livereload.listen();
+
+  // Watch any files in dist/, reload on change
+  gulp.watch(['dist/**']).on('change', livereload.changed);
+
+});
--- readme.md
+++ readme.md
@@ -5,4 +5,11 @@
 This health tracker is a single page web app. You can search for any foods or drinks a it will return search results with items and their calories.
 
 You can then add an items to a collection which automatically adds up all calories.
-The data is retrieved from the <a href="https://developer.nutritionix.com/docs/v1_1">nutritionix API</a>.
+The data is retrieved from the <a href="https://developer.nutritionix.com/docs/v1_1">Nutritionix API</a>.
+
+## Components
+
+This app is built using on <a href="">Backbonejs</a>, an organizational library.
+Further dependencies include:
+
+<a href=""></a>
