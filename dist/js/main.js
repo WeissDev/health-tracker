662a8450d6bed21a7794c1d002391b48e9d8665e
WeissDev <valentin.weiss@hotmail.com>
2015-08-24 02:43:06 +0200

    bug fix

--- dist/js/main.js
+++ dist/js/main.js
@@ -35,6 +35,21 @@ var SelectedCollection = Backbone.Collection.extend({
 });
 
 app.SelectedCollection = new SelectedCollection();
+var app = app || {};
+
+/** FoodCollection to store search results */
+var FoodCollection = Backbone.Collection.extend({
+
+	/** Reference to this collections model */
+	model: app.FoodModel,
+
+	/** Save collection to localStorage under 'results-backbone' */
+	localStorage: new Backbone.LocalStorage('results-backbone')
+
+
+});
+
+app.FoodCollection = new FoodCollection();
 /**
  * VIEW
  */
